80e728f5ee575925302e33d0fa97cb9858f91824
OMR218 <omarmohamed124200@gmail.com>
2026-07-01 16:53:57 +0300

    sg edit 7

--- Terraform/modules/security-groups/outputs.tf
+++ Terraform/modules/security-groups/outputs.tf
@@ -2,10 +2,6 @@ output "nlb_sg_id" {
   value = aws_security_group.nlb.id
 }
 
-output "alb_sg_id" {
-  value = aws_security_group.nlb.id
-}
-
 output "node_sg_id" {
   value = aws_security_group.node.id
 }
